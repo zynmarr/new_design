63e050a542a1ed2ccca79275a9d639228f792a35
Yunul Fikri <fyunul@gmail.com>
2020-06-03 13:47:29 +0700

    some change

--- lib/Screens/Menu/home.dart
+++ lib/Screens/Menu/home.dart
@@ -36,204 +36,212 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
+            
             Container(
               height: MediaQuery.of(context).size.height,
-              child: ListView(
-                padding: EdgeInsets.only(
-                  left: 15,
-                  right: 15,
-                  top: 25,
-                ),
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+
                 children: <Widget>[
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Discover',
-                        style: TextStyle(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Discover',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontFamily: 'Viga',
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        child: Icon(
+                          Icons.sort,
+                          size: 28,
                           color: Colors.white,
-                          fontSize: 30,
-                          fontFamily: 'Viga',
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      GestureDetector(
-                        child: Container(
-                          child: Icon(
-                            Icons.sort,
-                            size: 28,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    height: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          spreadRadius: 0.6,
-                          blurRadius: 9,
-                          offset: Offset(2, 6),
-                        ),
-                      ],
                     ),
-                    child: Swiper(
-                      itemCount: users.length,
-                      itemBuilder: (context, index) {
-                        var x = users[index];
-                        return Cards(
-                          x.imageUrl,
-                          x.name,
-                          x.old,
-                          x.gender,
-                          x.jobs,
-                          x.location,
-                          x.verified,
-                        );
-                      },
-                      controller: controllerSwipe,
-                      physics: NeverScrollableScrollPhysics(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Row(
+                  ],
+                ),
+                  Expanded(
+                    
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        GestureDetector(
-                          onTap: () {
-                            print('Share');
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            height: 45.0,
-                            width: 45.0,
+                      
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            height: 300,
                             decoration: BoxDecoration(
-                              color: Colors.lightBlueAccent[100],
-                              borderRadius: BorderRadius.circular(100),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black54,
+                                  spreadRadius: 0.6,
+                                  blurRadius: 9,
+                                  offset: Offset(2, 6),
+                                ),
+                              ],
                             ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.lightBlueAccent,
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.share,
-                                  size: 20,
-                                  color: Colors.white,
+                            child: Swiper(
+                              itemCount: users.length,
+                              itemBuilder: (context, index) {
+                                var x = users[index];
+                                return Cards(
+                                  x.imageUrl,
+                                  x.name,
+                                  x.old,
+                                  x.gender,
+                                  x.jobs,
+                                  x.location,
+                                  x.verified,
+                                );
+                              },
+                              controller: controllerSwipe,
+                              physics: NeverScrollableScrollPhysics(),
+                            ),
+                          ),
+                          SizedBox(height: 25.0,),
+                          Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {
+                                print('Share');
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                height: 45.0,
+                                width: 45.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent[100],
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlueAccent,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.share,
+                                      size: 20,
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    controllerSwipe.next();
-                                  });
-                                  print('close');
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(5),
-                                  height: 65.0,
-                                  width: 65.0,
-                                  decoration: BoxDecoration(
-                                    color: Colors.redAccent[100],
-                                    borderRadius: BorderRadius.circular(100),
-                                  ),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.redAccent,
-                                      borderRadius: BorderRadius.circular(100),
-                                    ),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.clear,
-                                        size: 30,
-                                        color: Colors.white,
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        controllerSwipe.next();
+                                      });
+                                      print('close');
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      height: 65.0,
+                                      width: 65.0,
+                                      decoration: BoxDecoration(
+                                        color: Colors.redAccent[100],
+                                        borderRadius: BorderRadius.circular(100),
+                                      ),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.redAccent,
+                                          borderRadius: BorderRadius.circular(100),
+                                        ),
+                                        child: Center(
+                                          child: Icon(
+                                            Icons.clear,
+                                            size: 30,
+                                            color: Colors.white,
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    controllerSwipe.next();
-                                  });
-                                  print('Love');
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(5),
-                                  height: 80.0,
-                                  width: 80.0,
-                                  decoration: BoxDecoration(
-                                    color: Colors.pink[200],
-                                    borderRadius: BorderRadius.circular(100),
+                                  SizedBox(
+                                    width: 10,
                                   ),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.pinkAccent,
-                                      borderRadius: BorderRadius.circular(100),
-                                    ),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.favorite,
-                                        size: 35,
-                                        color: Colors.white,
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        controllerSwipe.next();
+                                      });
+                                      print('Love');
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      height: 80.0,
+                                      width: 80.0,
+                                      decoration: BoxDecoration(
+                                        color: Colors.pink[200],
+                                        borderRadius: BorderRadius.circular(100),
+                                      ),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.pinkAccent,
+                                          borderRadius: BorderRadius.circular(100),
+                                        ),
+                                        child: Center(
+                                          child: Icon(
+                                            Icons.favorite,
+                                            size: 35,
+                                            color: Colors.white,
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            print('Favorit');
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            height: 45.0,
-                            width: 45.0,
-                            decoration: BoxDecoration(
-                              color: Colors.lightBlueAccent[100],
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.lightBlueAccent,
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.grade,
-                                  size: 20,
-                                  color: Colors.white,
-                                ),
+                                ],
                               ),
                             ),
-                          ),
+                            GestureDetector(
+                              onTap: () {
+                                print('Favorit');
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                height: 45.0,
+                                width: 45.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent[100],
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlueAccent,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.grade,
+                                      size: 20,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
+                      )
+                        ],
+                      ),
+                      
                       ],
                     ),
                   ),
